52cc2b197faf63b898be887efb0864521260260f
ldg <ldg030201>
2025-10-09 17:28:15 +0900

    init

---
+++ src/test/java/sch/ldg/aimasterygame/AiMasteryGameApplicationTests.java
@@ -0,0 +1,13 @@
+package sch.ldg.aimasterygame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMasteryGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
