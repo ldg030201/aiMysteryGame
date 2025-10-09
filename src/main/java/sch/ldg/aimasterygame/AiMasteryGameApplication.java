e04d1e52d58ce638bd4792b4504142e0f94471c7
ldg <ldg8910@naver.com>
2025-10-09 17:28:15 +0900

    init

---
+++ src/main/java/sch/ldg/aimasterygame/AiMasteryGameApplication.java
@@ -0,0 +1,13 @@
+package sch.ldg.aimasterygame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMasteryGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiMasteryGameApplication.class, args);
+	}
+
+}
