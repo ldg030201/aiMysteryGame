b7c6902b818618a4daf54d3d2724d46f46c953fe
ldg <ldg030201>
2025-10-12 23:49:12 +0900

    ⚡️ AI 통신 부분 add.

---
+++ src/main/java/sch/ldg/aimasterygame/ai/dto/UserRequestDTO.java
@@ -0,0 +1,16 @@
+package sch.ldg.aimasterygame.ai.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class UserRequestDTO {
+    public String scenarioId;
+    public String playerInput;
+    public String npcId;
+    public List<String> knownClues;
+}
