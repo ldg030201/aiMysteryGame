276798cfde1989e013e63552423721f5e4ffdfa8
ldg <ldg030201>
2025-11-10 23:56:22 +0900

    📦️ build package.

--- startServer.bat
+++ startServer.bat
@@ -14,6 +14,6 @@ echo.
 set /p a=  Gemini API KEY ÀÔ·Â ÈÄ Enter : 
 
 cls
-java -DGEMINI_API_KEY=%a% -jar build\libs\aiMysteryGame-0.0.1-SNAPSHOT.jar
+java -DGEMINI_API_KEY=%a% -jar build\libs\aiMysteryGame-0.0.1.jar
 pause
 goto exit
